2677fc31d706f08d8ced596b153eff934c1610c1
beacastroalves <beacastro1008@gmail.com>
2025-06-27 10:58:50 +0100

    modules http

--- Curso-nodejs/index.js
+++ Curso-nodejs/index.js
@@ -1,7 +1,7 @@
 import { Person } from './person.js';
-import './modules/path.js';
-import './modules/fs.js';
-
+// import './modules/path.js';
+// import './modules/fs.js';
+import './modules/http.js';
 
 
 const person = new Person("Beatriz");
