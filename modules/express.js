6aaf7beb3a770ee584e5a66735661f22b1273097
beacastroalves <beacastro1008@gmail.com>
2025-06-27 19:31:48 +0100

    Atualizar e deletar por id

--- modules/express.js
+++ modules/express.js
@@ -39,6 +39,30 @@ app.post('/users', async (req, res) => {
   }
 });
 
+app.patch('/users/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndDelete(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando com Express na porta ${port}`))
