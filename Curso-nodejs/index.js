f2f428de3435198073f1ecf4669b8a444c481b8d
beacastroalves <beacastro1008@gmail.com>
2025-06-27 10:59:11 +0100

    modules express

--- Curso-nodejs/index.js
+++ Curso-nodejs/index.js
@@ -1,7 +1,8 @@
 import { Person } from './person.js';
 // import './modules/path.js';
 // import './modules/fs.js';
-import './modules/http.js';
+// import './modules/http.js';
+import './modules/express.js';
 
 
 const person = new Person("Beatriz");
---
+++ Curso-nodejs/modules/express.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/home', (req, res) => {
+  res.status(200).send('<h1>Helloooooo Wooooorld!</h1>');
+});
+
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      name: 'John Doe',
+      email: 'john@doe.com'
+    },
+    {
+      name: 'Jane Doe',
+      email: 'jane@doe.com'
+    }
+  ];
+
+  res.status(200).json(users);
+});
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Rodando com Express na porta ${port}`))
