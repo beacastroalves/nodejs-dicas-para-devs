047b7bb6a4d5a685b9fb3159287a5590988cf1c8
beacastroalves <beacastro1008@gmail.com>
2025-07-13 17:14:59 +0100

    React parte 2  - onAddTaskSubmit e id

--- fsw-aulao-react/src/components/Tasks.jsx
+++ fsw-aulao-react/src/components/Tasks.jsx
@@ -1,20 +1,20 @@
 import { ChevronRightIcon, Trash2Icon } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
 
   return (
     <ul className="space-y-4 p-4 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-          onClick={() => props.onTaskClick(task.id)}
+          onClick={() => onTaskClick(task.id)}
           className={`w-full bg-slate-400 text-white p-2 rounded-md text-left ${task.isCompleted && "line-through"}`}>
             {task.title}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
             <ChevronRightIcon />
           </button>
-          <button onClick={() => {props.onDeleteTaskClick(task.id)}} className="bg-slate-400 text-white p-2 rounded-md">
+          <button onClick={() => { onDeleteTaskClick(task.id)}} className="bg-slate-400 text-white p-2 rounded-md">
             <Trash2Icon />
           </button>
         </li>
