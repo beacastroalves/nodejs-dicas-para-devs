65b7c16bc7a278fb7f1d004a55557ce2efecb63f
beacastroalves <beacastro1008@gmail.com>
2025-08-15 20:35:22 +0100

    interfaces com funções

--- fsw-aulao-ts/src/functions.ts
+++ fsw-aulao-ts/src/functions.ts
@@ -1,10 +1,20 @@
-const sum = (x: number, y: number): string | number => {
-  return (x + y).toString();
+interface MathFunc {
+  (x: number, y: number): number;
+};
+
+const sum: MathFunc = (x: number, y: number): number => {
+  return x + y;
+};
+
+const sub: MathFunc = (x: number, y: number) => {
+  return x - y;
 };
 
 const value = sum(2, 5);
+const value2 = sub(3, 8);
 
 // void usado apenas para funções
 const log = (message: string): void => {
   console.log(message);
-}
+};
+
