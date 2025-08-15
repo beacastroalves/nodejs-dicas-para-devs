3ed22f63751e305587cc96f81d5773477e532029
beacastroalves <beacastro1008@gmail.com>
2025-08-15 12:35:31 +0100

    functions em ts

---
+++ fsw-aulao-ts/dist/functions.js
@@ -0,0 +1,9 @@
+const sum = (x, y) => {
+    return (x + y).toString();
+};
+const value = sum(2, 5);
+const log = (message) => {
+    console.log(message);
+};
+export {};
+//# sourceMappingURL=functions.js.map
